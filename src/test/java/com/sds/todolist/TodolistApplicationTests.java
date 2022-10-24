5093113fc4722eaf01428065aca9874461186a84
solsol96 <zmfpdlwm130@naver.com>
2022-10-24 11:31:50 +0900

    first commit

---
+++ src/test/java/com/sds/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
